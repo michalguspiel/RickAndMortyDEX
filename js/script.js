8ef1d220660cb0d5064ff7d25d8c6ea54029826e
guspi <guspielmichal@gmail.com>
2022-03-27 11:37:58 +0300

    Improved splash screen, added comments to code.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
 console.log("HELLO WORLD");
 
 var charactersUrl = "https://rickandmortyapi.com/api/character"
+
+/**
+ * Theese are two flags regarding splash screen.
+ * The intended behaviour is that splash screen should disappear only if content is loaded but not earlier than 1.2 seconds
+ * since the page load.
+ * 
+*/
 var isSplashScreenTimerGone = false
 var isContentLoaded = false
 
+/**
+ * Starts timer for splash screen on the page load.
+ * */ 
 var onLoad = function(){
   setTimeout(splashScreenTimeOut,1200)
 }
-
+/**
+ * Function called when the timer for splash screen has ended,
+ * if the flag for loaded content is already changed for true it will remove splash screen otherwise won't
+ */
 function splashScreenTimeOut(){
   isSplashScreenTimerGone = true
   if(isContentLoaded)removeSplashScreen()
 }
 
+/**
+ * Function which adds class display-none to splashs screen and therefore removes splash screen
+ */
 function removeSplashScreen(){
   var splashScreen = document.getElementById('splash')
   splashScreen.classList.add('display-none');
@@ -26,7 +42,7 @@ xmlhttp.onreadystatechange = function(){
 
     if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
       isContentLoaded = true
-      if(isSplashScreenTimerGone)removeSplashScreen
+      if(isSplashScreenTimerGone)removeSplashScreen // if splash screen timer has already passsed, splash screen will be removed from here
         var data = xmlhttp.responseText
         var json = JSON.parse(data)
         var results = json.results
